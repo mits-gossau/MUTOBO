0a399dc8e5ae3fbe3f49f26f4aa4697524799f41
Laurin-dev <laurin.graf@mgb.ch>
2022-03-25 10:17:29 +0100

    ImageHotpost View

---
+++ Dit.Umb.Mutobo/Extentions/HtmlHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Composing;
+
+namespace Dit.Umb.Mutobo.Extentions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString UmbracoTranslation(this HtmlHelper helper, string value)
+        {
+            return new HtmlString(Current.UmbracoHelper.GetDictionaryValue(value, "TRANSLATE: " + value));
+        }
+    }
+}
